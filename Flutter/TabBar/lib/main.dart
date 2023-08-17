f9eb8b6bd7f9d8450807f1bff0b128f36772924c
Joelmir R. Carvalho <joalves3carvalho@gmail.com>
2023-08-17 14:53:27 -0300

    tabbar

--- Flutter/TabBar/lib/main.dart
+++ Flutter/TabBar/lib/main.dart
@@ -6,40 +6,62 @@ void main() {
   ));
 }
 
-
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
   @override
-  State<MyApp> createState() =>  _MyAppState();
+  State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> with TickerProviderStateMixin {
- late TabController _tabController;
+  late TabController _tabController;
 
   @override
   void initState() {
     super.initState();
-    _tabController =  TabController(length: 2, vsync: this);
+    _tabController = TabController(length: 2, vsync: this);
   }
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar:  AppBar(
-        title:  const Text('Meu App'),
-        bottom:  TabBar(
-          tabs: const <Tab>[
-             Tab(text: 'Home', icon: Icon(Icons.home),),
-             Tab(text: 'Configuração', icon: Icon(Icons.settings),),
-          ],
-          controller: _tabController,
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Meu App'),
       ),
-      body:  TabBarView(
-        controller: _tabController,
-        children: const <Widget>[
-           Center(child:  Text('Home')),
-           Center(child:  Text('Configuração')),
+      body: Column(
+        children: [
+          Container(
+            color: Colors.indigo,
+            child: TabBar(
+              indicatorColor: Colors.white,
+              tabs: const <Tab>[
+                Tab(
+                  text: 'Home',
+                  icon: Icon(Icons.home),
+                ),
+                Tab(
+                  text: null,
+                  icon: null,
+                  child: Row(
+                    children: [
+                      Icon(Icons.settings),
+                      SizedBox(width: 10),
+                      Text("Configuração"),
+                    ],
+                  ),
+                ),
+              ],
+              controller: _tabController,
+            ),
+          ),
+          Expanded(
+            child: TabBarView(
+              controller: _tabController,
+              children: const <Widget>[
+                Center(child: Text('Home')),
+                Center(child: Text('Configuração')),
+              ],
+            ),
+          ),
         ],
       ),
     );
